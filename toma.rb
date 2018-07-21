61f84e6b767cfaecb2760e3f3fcae1addbd1f00a
Maxim Lapunin <majagyage@gmail.com>
2018-07-22 01:22:27 +0300

    changes

--- toma.rb
+++ toma.rb
@@ -1,9 +1,25 @@
 #!/usr/bin/ruby
 
-@alarm_thread = @timer_thread = Thread.new {}
+@tick_thread = @alarm_thread = @timer_thread = Thread.new {}
+
+def start_tick
+  return if @tick_thread.alive?
+  @tick_thread = Thread.new do
+    loop do      
+      system "beep -f 1000 -l 1 >/dev/null"
+      sleep 1
+    end
+  end
+end
+
+def stop_tick
+  @tick_thread.kill
+  sleep 0.1 while @tick_thread.alive?
+end
 
 def start_alarm
   return if @alarm_thread.alive?
+  stop_tick
   @alarm_thread = Thread.new do
     loop do      
       system "beep -f 5000 -l 300 >/dev/null"
@@ -14,10 +30,12 @@ end
 
 def stop_alarm
   @alarm_thread.kill
+  sleep 0.1 while @alarm_thread.alive?
+  start_tick
 end
 
 def log start_date, start_time, end_time
-  text = `zenity --entry --title='Toma #{start_time}-#{end_time}' --text='What are you doing?'`
+  text = `zenity --entry --title='Toma #{start_time}-#{end_time}' --text='What are you doing?' 2>/dev/null`
   File.open(ENV['HOME']+"/toma/#{start_date}.log", 'a') do |file|
     file.write "#{start_time}-#{end_time} #{text.split.first}\n"
   end
@@ -27,6 +45,7 @@ def start_timer delay
   return if @timer_thread.alive?
   @timer_thread = Thread.new do
     stop_alarm
+    stop_tick
     time = Time.now
     start_date = time.strftime("%Y-%m-%d")
     start_time = time.strftime("%H:%M")
@@ -52,6 +71,8 @@ end
 
 def input
   puts 'Hello!'
+  stop_tick
+  start_tick
 
   case @inp = gets.chomp
   when /start/
@@ -59,6 +80,8 @@ def input
 
   when /stop/
     stop_timer
+    stop_alarm
+    start_tick
     puts 'Alarm stopped!'
 
   when /next/
@@ -68,6 +91,10 @@ def input
   when /exit/
     puts 'Good-bye!'
     return false
+
+  when /tick/
+    stop_alarm
+    start_tick
 
   when /status/
     puts "Alarm status: \t" + (@alarm_thread.alive? ? 'True' : 'False')
